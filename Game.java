4dd7a0a78dce6ac5e67bda3525c776375153f801
MalavikaAjit <malavikaajit9@gmail.com>
2021-03-24 13:51:18 +0530

    ADD. added condition to reach winning position

--- Game.java
+++ Game.java
@@ -1,23 +1,50 @@
 import java.util.Random;
 public class Game {
        
+    public static final int InitialPosition = 0;
+    public static final int FinalPosition = 100;
+    
     public static int RollADie() {
-        Random random = new Random();
-        int number = random.nextInt(6)+1;
-        return number;
+       Random random = new Random();
+       int number = random.nextInt(6)+1;
+       return number;
+    }
+    public static int gameOption() {
+       Random random = new Random();
+       int option= random.nextInt(3);
+       return option;
     }
     public static void main(String[] args) {
-         System.out.println(" welcome to snake and ladder game  ");	
-         int position =0;
-         System.out.println("Initial position" +position);
-         int diceNumber = RollADie();
-         System.out.println("dice number is " + diceNumber);  
-    
-    }
+        System.out.println(" welcome to snake and ladder game  ");
+        int position= 0;
+        while (position < FinalPosition) {
+           int diceNumber = RollADie();
+           System.out.println("dice number is " + diceNumber);
+           int choice = gameOption();
+           switch(choice) {
+             case 0:
+               System.out.println("No play; player stays " );
+               position = position;
+               break;
+             case 1:
+               System.out.println("ladder; the player moves ahead ");
+               if(position - diceNumber < FinalPosition){
+                   position = position + diceNumber;
+               }
+               break; 
+             case 2:
+               System.out.println("Snake; the player moves backward " );
+                   position = position - diceNumber;
+                   if(position < InitialPosition) {
+                   position = InitialPosition;
+               }
+               break;
+           }
+            System.out.println("current position" +position);
+        }   
+   }
+     
 }
-
-
-
 
 
 
