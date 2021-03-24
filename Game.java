cd6b83a2bedac9d46731a836991986e5fe1fcb55
MalavikaAjit <malavikaajit9@gmail.com>
2021-03-24 13:16:20 +0530

    ADD. added feature rolled a die

--- Game.java
+++ Game.java
@@ -1,15 +1,18 @@
+import java.util.Random;
 public class Game {
        
-       
-      
-     
-      
-        public static void main(String[] args) {
-         System.out.println(" welcome to snake and ladder game  ");
-         int position =0;
+    public static int RollADie() {
+        Random random = new Random();
+        int number = random.nextInt(6)+1;
+        return number;
+    }
+    public static void main(String[] args) {
+         System.out.println(" welcome to snake and ladder game  ");	int position =0;
          System.out.println("Initial position" +position);
-           
-     }
+         int diceNumber = RollADie();
+         System.out.println("dice number is " + diceNumber);  
+    
+    }
 }
 
 
