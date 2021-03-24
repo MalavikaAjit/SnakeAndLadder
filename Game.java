c2fe3775f53e3cb7bdd872d3f0e4ec8264e58f5c
MalavikaAjit <malavikaajit9@gmail.com>
2021-03-24 12:41:43 +0530

    ADD. added a welcome message

---
+++ Game.java
@@ -0,0 +1,21 @@
+public class Game {
+       
+       
+      
+     
+      
+        public static void main(String[] args) {
+         System.out.println(" welcome to snake and ladder game  ");
+         int position =0;
+         System.out.println("Initial position" +position);
+           
+     }
+}
+
+
+
+
+
+
+
+            
